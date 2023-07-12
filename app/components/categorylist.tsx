6f07a4ccfcb64f44e037eb8f2e69983a0cf16ac3
awsle <asaadsleman@mail.tau.ac.il>
2023-07-12 18:33:57 +0300

    ui edited

--- app/components/categorylist.tsx
+++ app/components/categorylist.tsx
@@ -5,7 +5,13 @@ import { CategoryData } from "../types";
 export default function CategoriesList({ data }: { data: CategoryData }) {
     return (
         <div className="pl-10 pr-15 w-[70rem]">
-            <label className="text-indigo-600 text-3xl mb-1.5"><b>Categories</b></label>
+            <div className="flex items-end">
+                <label className="text-indigo-600 text-3xl mb-1.5">Categories</label>
+                <p className="text-gray-500 pl-3 mb-1 text-xl">
+                    Generate learning experiences with Curated AI for your teams' needs!
+                </p>
+
+            </div>
             <div className="flex flex-row mt-3 overflow-scroll">
                 <ul className="flex flex-row shrink-0">
                     {data && data.topics.map((topic) =>
